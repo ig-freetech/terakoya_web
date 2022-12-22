f5841522c05ee23617c79c36d28a3807e111f4cc
I.G <i.g.freetech2021@gmail.com>
2022-12-23 04:14:23 +0900

    Modify webpack config path

--- app/webpack.common.js
+++ app/webpack.common.js
@@ -2,14 +2,13 @@ const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 // const BomPlugin = require("webpack-utf8-bom"); // Build時以外 [webpack-dev-middleware] Error: ENOENT: no such file or directory, open main.XXX.hot-update.js の原因となるためコメントアウト
 
-const PUBLIC_DIR = path.resolve(__dirname, "public");
 const APP_DIR = path.resolve(__dirname, "src", "app");
 
 /**@type import('webpack').Configuration */
 module.exports = {
   entry: path.join(APP_DIR, "index.tsx"),
   output: {
-    path: PUBLIC_DIR,
+    path: path.resolve(__dirname, "public"),
     filename: "index.js",
   },
   module: {
