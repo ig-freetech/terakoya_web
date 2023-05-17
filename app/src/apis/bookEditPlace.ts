e20caf26e25f7e5b8512f7b4a708e9e2a77fe236
I.G <i.g.freetech2021@gmail.com>
2023-05-17 01:44:22 +0000

    Fix as a result of Serverless framework installed

--- app/src/apis/bookEditPlace.ts
+++ app/src/apis/bookEditPlace.ts
@@ -3,4 +3,4 @@ import { BASE_URL } from "@apis/config";
 import { BookingItem } from "@apis/bookList";
 
 export const editBookingPlace = (item: BookingItem) =>
-  axios.put(BASE_URL + "/book/edit/place", item);
+  axios.put(BASE_URL + "/booking/edit/place", item);
--- app/src/apis/config.ts
+++ app/src/apis/config.ts
@@ -5,4 +5,4 @@ const API_GATEWAY_ID =
 
 const API_GATEWAY_REGION = process.env.AWS_GATEWAY_REGION;
 
-export const BASE_URL = `https://${API_GATEWAY_ID}.execute-api.${API_GATEWAY_REGION}.amazonaws.com/v1`;
+export const BASE_URL = `https://${API_GATEWAY_ID}.execute-api.${API_GATEWAY_REGION}.amazonaws.com`;
