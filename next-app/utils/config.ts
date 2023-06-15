10f75ceb9c20851d00c4cd30d9fdddbdb3f237e4
I.G <i.g.freetech2021@gmail.com>
2023-06-15 15:44:25 +0000

    Modify github workflows and env values

--- next-app/utils/config.ts
+++ next-app/utils/config.ts
@@ -6,7 +6,3 @@ const API_GATEWAY_ID =
     : process.env.AWS_PROD_GATEWAY_ID;
 
 export const API_BASE_URL = `https://${API_GATEWAY_ID}.execute-api.${AWS_DEFAULT_REGION}.amazonaws.com`;
-
-const TERAKOYA_BUCKET_NAME = process.env.TERAKOYA_BUCKET_NAME;
-
-export const ASSETS_BASE_URL = `https://${TERAKOYA_BUCKET_NAME}.s3.${AWS_DEFAULT_REGION}.amazonaws.com/web/assets`;
