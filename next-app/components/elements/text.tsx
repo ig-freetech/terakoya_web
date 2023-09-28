9dad9c5f69c557054e0dcffac88bd89278abb8e6
I.G <i.g.freetech2021@gmail.com>
2023-09-28 17:15:26 +0000

    feat: add TextIndigo

--- next-app/components/elements/text.tsx
+++ next-app/components/elements/text.tsx
@@ -55,6 +55,9 @@ export const TextDarkBrown = styled.span`
 export const TextLightBrown = styled.span`
   color: ${colors.lightBrown};
 `;
+export const TextIndigo = styled.span`
+  color: ${colors.indigo};
+`;
 export const SmallTextGray = styled.span`
   font-size: 12px;
   color: ${colors.gray};
